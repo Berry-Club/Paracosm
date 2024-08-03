8071158c2f4d804422ce7a4ac0e08a12d681fbd1
Aaron Howser <aaronhowser1@gmail.com>
2024-08-03 18:56:56 -0500

    creative tab

---
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModCreativeTabs.kt
@@ -0,0 +1,27 @@
+package dev.aaronhowser.mods.paracosm.registry
+
+import dev.aaronhowser.mods.paracosm.Paracosm
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.world.item.CreativeModeTab
+import net.neoforged.neoforge.registries.DeferredHolder
+import net.neoforged.neoforge.registries.DeferredItem
+import net.neoforged.neoforge.registries.DeferredRegister
+import java.util.function.Supplier
+
+object ModCreativeTabs {
+
+    val CREATIVE_TAB_REGISTRY: DeferredRegister<CreativeModeTab> =
+        DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, Paracosm.ID)
+
+    val MOD_TAB: DeferredHolder<CreativeModeTab, CreativeModeTab> =
+        CREATIVE_TAB_REGISTRY.register("pitch_perfect", Supplier {
+            CreativeModeTab.builder()
+                .title(ModLanguageProvider.Misc.CREATIVE_TAB.toComponent())
+                .icon { ModItems.COTTON.toStack() }
+                .displayItems { a, b ->
+                    b.acceptAll(ModItems.ITEM_REGISTRY.entries.map { (it as DeferredItem).toStack() })
+                }
+                .build()
+        })
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModItems.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModItems.kt
@@ -1,11 +1,16 @@
 package dev.aaronhowser.mods.paracosm.registry
 
 import dev.aaronhowser.mods.paracosm.Paracosm
+import net.minecraft.world.item.Item
+import net.neoforged.neoforge.registries.DeferredItem
 import net.neoforged.neoforge.registries.DeferredRegister
 
 object ModItems {
 
     val ITEM_REGISTRY: DeferredRegister.Items =
         DeferredRegister.createItems(Paracosm.ID)
+
+    val COTTON: DeferredItem<Item> =
+        ITEM_REGISTRY.registerSimpleItem("cotton")
 
 }
