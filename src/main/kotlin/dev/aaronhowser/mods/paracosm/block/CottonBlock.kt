4a5721c72892d283555a3f8da1f157e2ca886897
Aaron Howser <aaronhowser1@gmail.com>
2024-08-03 20:17:56 -0500

    logic

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/block/CottonBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/block/CottonBlock.kt
@@ -2,11 +2,18 @@ package dev.aaronhowser.mods.paracosm.block
 
 import com.mojang.serialization.MapCodec
 import dev.aaronhowser.mods.paracosm.registry.ModItems
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.block.CropBlock
 import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.IntegerProperty
 import net.minecraft.world.level.material.MapColor
 import net.minecraft.world.level.material.PushReaction
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class CottonBlock(
     properties: Properties = Properties.of()
@@ -20,6 +27,26 @@ class CottonBlock(
 
     companion object {
         val CODEC: MapCodec<CottonBlock> = simpleCodec(::CottonBlock)
+
+        val AGE: IntegerProperty = BlockStateProperties.AGE_3
+        val SHAPE_BY_AGE = arrayOf(
+            box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
+            box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            box(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0)
+        )
+    }
+
+    override fun getAgeProperty(): IntegerProperty {
+        return AGE
+    }
+
+    override fun getMaxAge(): Int {
+        return 3
+    }
+
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+        return SHAPE_BY_AGE[getAge(state)]
     }
 
     override fun codec(): MapCodec<out CropBlock> {
