e76abdcc02ce889d0a50217da0befc282a505df6
Aaron Howser <aaronhowser1@gmail.com>
2024-08-03 20:45:06 -0500

    datagen loot

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/ModDataGen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/ModDataGen.kt
@@ -1,16 +1,14 @@
 package dev.aaronhowser.mods.paracosm.datagen
 
 import dev.aaronhowser.mods.paracosm.Paracosm
+import dev.aaronhowser.mods.paracosm.datagen.loot.ModLootTableProvider
 import dev.aaronhowser.mods.paracosm.datagen.model.ModBlockStateProvider
 import dev.aaronhowser.mods.paracosm.datagen.model.ModItemModelProvider
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.DataGenerator
 import net.minecraft.data.PackOutput
-import net.minecraft.data.loot.LootTableProvider
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
-import net.neoforged.neoforge.common.data.AdvancementProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import net.neoforged.neoforge.data.event.GatherDataEvent
 import java.util.concurrent.CompletableFuture
@@ -29,50 +27,28 @@ object ModDataGen {
         val lookupProvider: CompletableFuture<HolderLookup.Provider> = event.lookupProvider
 
         val languageProvider = generator.addProvider(event.includeClient(), ModLanguageProvider(output))
-
-        val blockStateProvider = generator.addProvider(
-            event.includeClient(),
-            ModBlockStateProvider(output, existingFileHelper)
-        )
-
         val itemModelProvider = generator.addProvider(
             event.includeClient(),
             ModItemModelProvider(output, existingFileHelper)
         )
-//
-//        val recipeProvider = generator.addProvider(
-//            event.includeServer(),
-//            ModRecipeProvider(output, lookupProvider)
-//        )
-//
+        val blockModelProvider = generator.addProvider(
+            event.includeClient(),
+            ModBlockStateProvider(output, existingFileHelper)
+        )
+
+//        val recipeProvider = generator.addProvider(event.includeServer(), ModRecipeProvider(output, lookupProvider))
+
 //        val blockTagProvider = generator.addProvider(
 //            event.includeServer(),
 //            ModBlockTagsProvider(output, lookupProvider, existingFileHelper)
 //        )
-//
 //        val itemTagProvider = generator.addProvider(
 //            event.includeServer(),
 //            ModItemTagsProvider(output, lookupProvider, blockTagProvider.contentsGetter(), existingFileHelper)
 //        )
-//
 //        val entityTypeTagProvider = generator.addProvider(
 //            event.includeServer(),
 //            ModEntityTypeTagsProvider(output, lookupProvider, existingFileHelper)
-//        )
-//
-//        val blockLootTableProvider = generator.addProvider(
-//            event.includeServer(),
-//            LootTableProvider(
-//                output,
-//                setOf(),
-//                listOf(
-//                    LootTableProvider.SubProviderEntry(
-//                        ::ModBlockLootTableSubProvider,
-//                        LootContextParamSets.BLOCK
-//                    )
-//                ),
-//                lookupProvider
-//            )
 //        )
 //
 //        val advancementProvider = generator.addProvider(
@@ -84,6 +60,11 @@ object ModDataGen {
 //                listOf(ModAdvancementSubProvider())
 //            )
 //        )
+
+        val lootTableProvider = generator.addProvider(
+            event.includeServer(),
+            ModLootTableProvider.create(output, lookupProvider)
+        )
 
     }
 
---
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/loot/ModBlockLootTablesSubProvider.kt
@@ -0,0 +1,50 @@
+package dev.aaronhowser.mods.paracosm.datagen.loot
+
+import dev.aaronhowser.mods.paracosm.block.CottonBlock
+import dev.aaronhowser.mods.paracosm.registry.ModBlocks
+import dev.aaronhowser.mods.paracosm.registry.ModItems
+import net.minecraft.advancements.critereon.StatePropertiesPredicate
+import net.minecraft.core.HolderLookup
+import net.minecraft.data.loot.BlockLootSubProvider
+import net.minecraft.world.flag.FeatureFlags
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition
+
+class ModBlockLootTablesSubProvider(
+    provider: HolderLookup.Provider
+) : BlockLootSubProvider(setOf(), FeatureFlags.REGISTRY.allFlags(), provider) {
+
+    override fun generate() {
+        dropSelf(ModBlocks.NIGHT_LIGHT.get())
+
+        cotton()
+    }
+
+    private fun cotton() {
+        val block = ModBlocks.COTTON.get()
+        val builder = LootItemBlockStatePropertyCondition
+            .Builder(block)
+            .setProperties(
+                StatePropertiesPredicate.Builder.properties().hasProperty(CottonBlock.AGE, 3)
+            )
+
+        add(
+            block,
+            createCropDrops(
+                block,
+                ModItems.COTTON.get(),
+                ModItems.COTTON.get(),
+                builder
+            )
+        )
+
+    }
+
+    override fun getKnownBlocks(): List<Block> {
+        return listOf(
+            ModBlocks.NIGHT_LIGHT.get(),
+            ModBlocks.COTTON.get()
+        )
+    }
+
+}
