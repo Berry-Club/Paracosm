ecce5630758b43a351d2990eff377b0a3f2e69e9
Aaron Howser <aaronhowser1@gmail.com>
2024-08-05 22:36:07 -0500

    TowelCapeItem start

---
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/TowelCapeItem.kt
@@ -0,0 +1,10 @@
+package dev.aaronhowser.mods.paracosm.item
+
+import net.minecraft.world.item.ElytraItem
+import net.minecraft.world.item.Rarity
+
+class TowelCapeItem : ElytraItem(
+    Properties()
+        .durability(432)
+        .rarity(Rarity.UNCOMMON)
+)
