5e534572e457128019f6047365c700fda7cae138
Aaron Howser <aaronhowser1@gmail.com>
2024-08-05 22:36:10 -0500

    Revert "ToyItem"
    This reverts commit 70e9b6fce0a9e9be445c38814f11d78c228fd534.

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/ToyGunItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/ToyGunItem.kt
@@ -1,10 +1,12 @@
 package dev.aaronhowser.mods.paracosm.item
 
-import dev.aaronhowser.mods.paracosm.item.base.ToyItem
+import dev.aaronhowser.mods.paracosm.attachment.RequiresWhimsy
+import dev.aaronhowser.mods.paracosm.util.ClientUtil
 import net.minecraft.network.chat.Component
+import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 
-class ToyGunItem : ToyItem(
+class ToyGunItem : RequiresWhimsy, Item(
     Properties()
         .stacksTo(1)
 ) {
@@ -12,12 +14,13 @@ class ToyGunItem : ToyItem(
     override val requiredWhimsy: Float = 5f
 
     override fun getName(stack: ItemStack): Component {
-        return if (true /* Some value that changes depending on who's reading the tooltip */) {
+        return if (ClientUtil.whimsy >= requiredWhimsy) {
             Component.literal("Super cool gun")
         } else {
             Component.literal("Toy gun")
         }
     }
+
 
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/base/ToyItem.kt
+++
@@ -1,8 +0,0 @@
-package dev.aaronhowser.mods.paracosm.item.base
-
-import dev.aaronhowser.mods.paracosm.attachment.RequiresWhimsy
-import net.minecraft.world.item.Item
-
-abstract class ToyItem(
-    properties: Properties
-) : Item(properties), RequiresWhimsy
