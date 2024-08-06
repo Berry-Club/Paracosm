e1ca258c1998f32556f13f7487c6490c677cddea
Aaron Howser <aaronhowser1@gmail.com>
2024-08-05 21:30:44 -0500

    OtherUtil.isLookingAtPos()

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/util/OtherUtil.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/util/OtherUtil.kt
@@ -2,10 +2,29 @@ package dev.aaronhowser.mods.paracosm.util
 
 import dev.aaronhowser.mods.paracosm.Paracosm
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.phys.Vec3
+import kotlin.math.acos
 
 object OtherUtil {
 
     fun modResource(path: String): ResourceLocation =
         ResourceLocation.fromNamespaceAndPath(Paracosm.ID, path)
+
+    fun isLookingAtPos(
+        livingEntity: LivingEntity,
+        pos: Vec3,
+        angleDegrees: Float
+    ): Boolean {
+        val lookVec = livingEntity.lookAngle
+
+        val entityVec = livingEntity.eyePosition
+        val toPosVec = pos.subtract(entityVec).normalize()
+
+        val angleRad = acos(lookVec.dot(toPosVec))
+        val angle = Math.toDegrees(angleRad)
+
+        return angle < angleDegrees
+    }
 
 }
