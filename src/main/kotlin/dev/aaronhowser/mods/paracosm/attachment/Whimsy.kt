a7aa5f53c09fcab794e37875341e040413cbcf48
Aaron Howser <aaronhowser1@gmail.com>
2024-08-05 22:07:45 -0500

    whimsy packet send

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/attachment/Whimsy.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/attachment/Whimsy.kt
@@ -1,7 +1,10 @@
 package dev.aaronhowser.mods.paracosm.attachment
 
 import com.mojang.serialization.Codec
+import dev.aaronhowser.mods.paracosm.packet.ModPacketHandler
+import dev.aaronhowser.mods.paracosm.packet.server_to_client.UpdateWhimsyValue
 import dev.aaronhowser.mods.paracosm.registry.ModAttachmentTypes
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.LivingEntity
 
 data class Whimsy(
@@ -19,6 +22,16 @@ data class Whimsy(
             get() = this.getData(ModAttachmentTypes.WHIMSY).amount
             set(value) {
                 this.setData(ModAttachmentTypes.WHIMSY, Whimsy(value))
+
+                val level = this.level()
+                if (level is ServerLevel) {
+                    ModPacketHandler.messageNearbyPlayers(
+                        UpdateWhimsyValue(this.id, value, true),
+                        level,
+                        this.position(),
+                        128.0
+                    )
+                }
             }
 
     }
