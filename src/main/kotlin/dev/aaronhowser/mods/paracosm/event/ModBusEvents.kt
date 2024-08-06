2e598f2c9473afaa4c127e35700033720ad4f2d5
Aaron Howser <aaronhowser1@gmail.com>
2024-08-05 22:08:29 -0500

    bus event

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/event/ModBusEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/event/ModBusEvents.kt
@@ -2,10 +2,12 @@ package dev.aaronhowser.mods.paracosm.event
 
 import dev.aaronhowser.mods.paracosm.Paracosm
 import dev.aaronhowser.mods.paracosm.entity.custom.TeddyBearEntity
+import dev.aaronhowser.mods.paracosm.packet.ModPacketHandler
 import dev.aaronhowser.mods.paracosm.registry.ModEntityTypes
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
 
 @EventBusSubscriber(
     modid = Paracosm.ID,
@@ -14,10 +16,13 @@ import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent
 object ModBusEvents {
 
     @SubscribeEvent
+    fun registerPayloads(event: RegisterPayloadHandlersEvent) {
+        ModPacketHandler.registerPayloads(event)
+    }
+
+    @SubscribeEvent
     fun entityAttributeEvent(event: EntityAttributeCreationEvent) {
-
         event.put(ModEntityTypes.TEDDY_BEAR.get(), TeddyBearEntity.setAttributes())
-
     }
 
 }
