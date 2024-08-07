4171972ad5a7844c6a5188e589cf6aa3d40798df
Aaron Howser <aaronhowser1@gmail.com>
2024-08-06 19:14:42 -0500

    block tags

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModBlockTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModBlockTagsProvider.kt
@@ -1,8 +1,10 @@
 package dev.aaronhowser.mods.paracosm.datagen.tag
 
 import dev.aaronhowser.mods.paracosm.Paracosm
+import dev.aaronhowser.mods.paracosm.registry.ModBlocks
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
+import net.minecraft.tags.BlockTags
 import net.neoforged.neoforge.common.data.BlockTagsProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.concurrent.CompletableFuture
@@ -14,6 +16,12 @@ class ModBlockTagsProvider(
 ) : BlockTagsProvider(output, lookupProvider, Paracosm.ID, existingFileHelper) {
 
     override fun addTags(pProvider: HolderLookup.Provider) {
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(
+            ModBlocks.NIGHT_LIGHT.get()
+        )
+
+        this.tag(BlockTags.CROPS).add(ModBlocks.COTTON.get())
+        this.tag(BlockTags.PRESSURE_PLATES).add(ModBlocks.WHOOPEE_CUSHION.get())
 
     }
 }
