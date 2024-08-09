c2ba9421a1e5262004dff6a4309deaaac0f47b50
Aaron Howser <aaronhowser1@gmail.com>
2024-08-08 19:20:47 -0500

    lang

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/ModLanguageProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/ModLanguageProvider.kt
@@ -25,6 +25,7 @@ class ModLanguageProvider(output: PackOutput) : LanguageProvider(output, Paracos
     object Entity {
         const val TEDDY_BEAR = "entity.paracosm.teddy_bear"
         const val DODGEBALL = "entity.paracosm.dodgeball"
+        const val STRING_WORM = "entity.paracosm.string_worm"
     }
 
     object Misc {
@@ -52,6 +53,7 @@ class ModLanguageProvider(output: PackOutput) : LanguageProvider(output, Paracos
 
         add(Entity.TEDDY_BEAR, "Teddy Bear")
         add(Entity.DODGEBALL, "Dodgeball")
+        add(Entity.STRING_WORM, "String Worm")
 
         add(Misc.CREATIVE_TAB, "Paracosm")
 
