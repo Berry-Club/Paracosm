92c92b275f2f22492a3b86bc27bcd132300e0c00
Aaron Howser <aaronhowser1@gmail.com>
2024-08-12 03:17:27 -0500

    Improving animation?

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/StringWormEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/StringWormEntity.kt
@@ -66,9 +66,7 @@ open class StringWormEntity(
     }
 
     private fun predicate(animationState: AnimationState<StringWormEntity>): PlayState {
-        val animationName = if (isHiding) {
-            return PlayState.STOP
-        } else if (animationState.isMoving) {
+        val animationName = if (animationState.isMoving) {
             "animation.stringworm.slither"
         } else return PlayState.STOP
 
