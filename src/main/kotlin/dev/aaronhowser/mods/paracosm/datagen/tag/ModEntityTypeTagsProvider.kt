04c3edbf5a780e94c18c360a5efb27a373e1f61e
Aaron Howser <aaronhowser1@gmail.com>
2024-08-16 13:24:29 -0500

    toys entity type tag

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModEntityTypeTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModEntityTypeTagsProvider.kt
@@ -2,10 +2,15 @@ package dev.aaronhowser.mods.paracosm.datagen.tag
 
 import dev.aaronhowser.mods.paracosm.Paracosm
 import dev.aaronhowser.mods.paracosm.registry.ModEntityTypes
+import dev.aaronhowser.mods.paracosm.util.OtherUtil
 import net.minecraft.core.HolderLookup
+import net.minecraft.core.registries.Registries
 import net.minecraft.data.PackOutput
 import net.minecraft.data.tags.EntityTypeTagsProvider
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.EntityTypeTags
+import net.minecraft.tags.TagKey
+import net.minecraft.world.entity.EntityType
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.concurrent.CompletableFuture
 
@@ -15,10 +20,25 @@ class ModEntityTypeTagsProvider(
     existingFileHelper: ExistingFileHelper?
 ) : EntityTypeTagsProvider(pOutput, pProvider, Paracosm.ID, existingFileHelper) {
 
+    companion object {
+        private fun create(name: String): TagKey<EntityType<*>> = create(OtherUtil.modResource(name))
+        private fun create(rl: ResourceLocation): TagKey<EntityType<*>> = TagKey.create(Registries.ENTITY_TYPE, rl)
+
+        val TOYS = create("toys")
+    }
+
     override fun addTags(pProvider: HolderLookup.Provider) {
 
         this.tag(EntityTypeTags.REDIRECTABLE_PROJECTILE)
             .add(ModEntityTypes.DODGEBALL.get())
+
+        this.tag(TOYS)
+            .add(
+                ModEntityTypes.AARONBERRY.get(),
+                ModEntityTypes.TEDDY_BEAR.get(),
+                ModEntityTypes.STRING_WORM.get(),
+                ModEntityTypes.DODGEBALL.get()
+            )
 
     }
 
