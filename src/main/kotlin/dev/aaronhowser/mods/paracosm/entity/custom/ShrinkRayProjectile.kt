0a7e9c872700b63d032fd631f0dbe2abe958befa
Aaron Howser <aaronhowser1@gmail.com>
2024-08-20 16:01:27 -0500

    working on ShrinkRayprojectile

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/ShrinkRayProjectile.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/ShrinkRayProjectile.kt
@@ -1,15 +1,24 @@
 package dev.aaronhowser.mods.paracosm.entity.custom
 
+import dev.aaronhowser.mods.paracosm.attachment.ShrinkRayEffect.Companion.shrinkRayEffect
 import dev.aaronhowser.mods.paracosm.registry.ModEntityTypes
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.Component
 import net.minecraft.network.syncher.EntityDataAccessor
 import net.minecraft.network.syncher.EntityDataSerializers
 import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.ai.attributes.Attributes
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.projectile.Projectile
 import net.minecraft.world.level.Level
+import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.EntityHitResult
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
+import kotlin.math.abs
 
 class ShrinkRayProjectile(
     entityType: EntityType<out Projectile>,
@@ -42,7 +51,44 @@ class ShrinkRayProjectile(
 
         const val IS_GROW_NBT = "IsGrow"
         const val SHOOTER_UUID_NBT = "ShooterUUID"
+
+        private fun changeEntityScale(
+            target: LivingEntity,
+            scaleChange: Double,
+            changer: Player? = null
+        ): Boolean {
+            val scaleBefore = target.getAttributeValue(Attributes.SCALE)
+
+            target.shrinkRayEffect += scaleChange
+
+            if (abs(target.shrinkRayEffect) < 0.01) {
+                target.shrinkRayEffect = 0.0
+            }
+
+            val scaleAfter = target.getAttributeValue(Attributes.SCALE)
+
+            val scaleChanged = scaleBefore != scaleAfter
+
+            if (scaleChanged) {
+                target.refreshDimensions()
+
+                val entityName = target.name.string
+                val afterString = "%.2f".format(scaleAfter)
+                val changeMessage = Component.literal("$entityName scale effect changed to $afterString")
+
+                changer?.displayClientMessage(changeMessage, true)
+
+                if (changer != target && target is Player) {
+                    target.displayClientMessage(changeMessage, true)
+                }
+
+            }
+
+            return scaleChanged
+        }
     }
+
+
 
     override fun defineSynchedData(builder: SynchedEntityData.Builder) {
         builder.define(IS_GROW, isGrow)
@@ -63,6 +109,33 @@ class ShrinkRayProjectile(
         } else {
             Optional.empty()
         }
+    }
+
+    override fun onHitBlock(result: BlockHitResult) {
+        super.onHitBlock(result)
+        if (!level().isClientSide) {
+            remove(RemovalReason.DISCARDED)
+        }
+    }
+
+    override fun onHitEntity(result: EntityHitResult) {
+        super.onHitEntity(result)
+
+        val target = result.entity as? LivingEntity ?: return
+        val amount = if (isGrow) 0.1 else -0.1
+        val shooterUuid = shooterUuid.getOrNull()
+
+        val shooterPlayer = if (shooterUuid != null) {
+            level().players().find { it.uuid == shooterUuid }
+        } else {
+            null
+        }
+
+        changeEntityScale(target, amount, shooterPlayer)
+    }
+
+    override fun getDefaultGravity(): Double {
+        return 0.0
     }
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/ShrinkRayItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/ShrinkRayItem.kt
@@ -1,91 +1,45 @@
 package dev.aaronhowser.mods.paracosm.item
 
-import dev.aaronhowser.mods.paracosm.attachment.ShrinkRayEffect.Companion.shrinkRayEffect
-import net.minecraft.network.chat.Component
+import dev.aaronhowser.mods.paracosm.attachment.RequiresWhimsy
+import dev.aaronhowser.mods.paracosm.entity.custom.ShrinkRayProjectile
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
-import net.minecraft.world.InteractionResult
 import net.minecraft.world.InteractionResultHolder
-import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
-import kotlin.math.abs
 
-class ShrinkRayItem : Item(
+class ShrinkRayItem : RequiresWhimsy, Item(
     Properties()
         .stacksTo(1)
 ) {
 
-    companion object {
-
-        private fun changeScale(
-            entity: LivingEntity,
-            scaleChange: Double,
-            changer: Player? = null
-        ): Boolean {
-            val scaleBefore = entity.getAttributeValue(Attributes.SCALE)
-
-            entity.shrinkRayEffect += scaleChange
-
-            if (abs(entity.shrinkRayEffect) < 0.01) {
-                entity.shrinkRayEffect = 0.0
-            }
-
-            val scaleAfter = entity.getAttributeValue(Attributes.SCALE)
-
-            val scaleChanged = scaleBefore != scaleAfter
-
-            if (scaleChanged) {
-                entity.refreshDimensions()
-
-                val entityName = entity.name.string
-                val afterString = "%.2f".format(scaleAfter)
-                val changeMessage = Component.literal("$entityName scale effect changed to $afterString")
-
-                changer?.displayClientMessage(changeMessage, true)
-
-                if (changer != entity && entity is Player) {
-                    entity.displayClientMessage(changeMessage, true)
-                }
-
-            }
-
-            return scaleChanged
-        }
-
-    }
-
-    override fun interactLivingEntity(
-        stack: ItemStack,
-        player: Player,
-        interactionTarget: LivingEntity,
-        usedHand: InteractionHand
-    ): InteractionResult {
-        val changeAmount = if (player.isShiftKeyDown) -0.1 else 0.1
-
-        val scaleChanged = changeScale(interactionTarget, changeAmount, player)
-
-        return if (scaleChanged) {
-            InteractionResult.sidedSuccess(interactionTarget.level().isClientSide)
-        } else {
-            InteractionResult.PASS
-        }
-    }
-
     override fun use(level: Level, player: Player, usedHand: InteractionHand): InteractionResultHolder<ItemStack> {
         val usedStack = player.getItemInHand(usedHand)
 
-        val changeAmount = if (player.isShiftKeyDown) -0.1 else 0.1
-
-        val scaleChanged = changeScale(player, changeAmount)
-
-        return if (scaleChanged) {
-            InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
-        } else {
-            InteractionResultHolder.pass(usedStack)
+        if (hasEnoughWhimsy(player)) {
+            return InteractionResultHolder.fail(usedStack)
         }
+
+        if (player is ServerPlayer) {
+            val shrinkRayProjectile = ShrinkRayProjectile(player, player.isSecondaryUseActive)
+
+            shrinkRayProjectile.shootFromRotation(
+                player,
+                player.xRot,
+                player.yRot,
+                0.0F,
+                1.5F,
+                1.0F
+            )
+
+            level.addFreshEntity(shrinkRayProjectile)
+        }
+
+        return InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
     }
+
+    override val requiredWhimsy: Float = 10f
 
 }
