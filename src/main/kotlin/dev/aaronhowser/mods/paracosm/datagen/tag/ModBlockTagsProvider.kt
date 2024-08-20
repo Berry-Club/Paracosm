00856dada44e6eff09082953d3800bbbcd9e3ecc
Aaron Howser <aaronhowser1@gmail.com>
2024-08-20 18:39:51 -0500

    added reflective block tag

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModBlockTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/datagen/tag/ModBlockTagsProvider.kt
@@ -2,9 +2,15 @@ package dev.aaronhowser.mods.paracosm.datagen.tag
 
 import dev.aaronhowser.mods.paracosm.Paracosm
 import dev.aaronhowser.mods.paracosm.registry.ModBlocks
+import dev.aaronhowser.mods.paracosm.util.OtherUtil
 import net.minecraft.core.HolderLookup
+import net.minecraft.core.registries.Registries
 import net.minecraft.data.PackOutput
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.BlockTags
+import net.minecraft.tags.TagKey
+import net.minecraft.world.level.block.Block
+import net.neoforged.neoforge.common.Tags
 import net.neoforged.neoforge.common.data.BlockTagsProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.concurrent.CompletableFuture
@@ -15,10 +21,20 @@ class ModBlockTagsProvider(
     existingFileHelper: ExistingFileHelper?
 ) : BlockTagsProvider(output, lookupProvider, Paracosm.ID, existingFileHelper) {
 
+    companion object {
+        fun create(name: String): TagKey<Block> = create(OtherUtil.modResource(name))
+        fun create(rl: ResourceLocation): TagKey<Block> = TagKey.create(Registries.BLOCK, rl)
+
+        val REFLECTIVE = create("reflective")
+    }
+
     override fun addTags(pProvider: HolderLookup.Provider) {
-        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(
-            ModBlocks.NIGHT_LIGHT.get()
-        )
+
+        this.tag(REFLECTIVE)
+            .addTags(Tags.Blocks.GLASS_BLOCKS)
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+            .add(ModBlocks.NIGHT_LIGHT.get())
 
         this.tag(BlockTags.CROPS).add(ModBlocks.COTTON.get())
         this.tag(BlockTags.PRESSURE_PLATES).add(ModBlocks.WHOOPEE_CUSHION.get())
