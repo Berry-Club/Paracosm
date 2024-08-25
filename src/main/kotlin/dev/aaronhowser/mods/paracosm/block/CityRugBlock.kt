892f46136f643da55255976d8b8ee485d3014ba3
Aaron Howser <aaronhowser1@gmail.com>
2024-08-25 13:54:00 -0500

    improve placing

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/block/CityRugBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/block/CityRugBlock.kt
@@ -59,30 +59,59 @@ class CityRugBlock(
         val segment = state.getValue(SEGMENT)
         if (segment != 0) return
 
-        val facing = state.getValue(FACING)
-        val clockwise = facing.clockWise    // You place the top left, and it places the rest to the right
-
-        for (i in 1..3) {
-            val otherPos = pos.relative(clockwise, i)
-            val otherState = level.getBlockState(otherPos)
-
-            if (otherState.isEmpty) {
-                level.setBlockAndUpdate(otherPos, state.setValue(SEGMENT, i))
-            }
-        }
-
-        for (i in 4..7) {
-            val otherPos = pos.relative(clockwise, i - 4).relative(clockwise.clockWise, 1)
-            val otherState = level.getBlockState(otherPos)
-
-            if (otherState.isEmpty) {
-                level.setBlockAndUpdate(otherPos, state.setValue(SEGMENT, i))
-            }
-        }
-
+        placeAll(pos, level, state)
     }
 
     companion object {
+
+        fun canPlaceAll(pos: BlockPos, level: Level, facing: Direction): Boolean {
+            val clockwise = facing.clockWise
+
+            for (i in 1..3) {
+                val otherPos = pos.relative(clockwise, i)
+                val otherState = level.getBlockState(otherPos)
+
+                if (!otherState.canBeReplaced()) return false
+            }
+
+            for (i in 4..7) {
+                val otherPos = pos.relative(clockwise, i - 4).relative(clockwise.clockWise, 1)
+                val otherState = level.getBlockState(otherPos)
+
+                if (!otherState.canBeReplaced()) return false
+            }
+
+            return true
+        }
+
+        fun placeAll(pos: BlockPos, level: Level, blockState: BlockState): Boolean {
+            val direction = blockState.getValue(FACING)
+
+            if (!canPlaceAll(pos, level, direction)) return false
+
+            val clockwise = direction.clockWise    // You place the top left, and it places the rest to the right
+
+            for (i in 1..3) {
+                val otherPos = pos.relative(clockwise, i)
+                val otherState = level.getBlockState(otherPos)
+
+                if (otherState.isEmpty) {
+                    level.setBlockAndUpdate(otherPos, blockState.setValue(SEGMENT, i))
+                }
+            }
+
+            for (i in 4..7) {
+                val otherPos = pos.relative(clockwise, i - 4).relative(clockwise.clockWise, 1)
+                val otherState = level.getBlockState(otherPos)
+
+                if (otherState.isEmpty) {
+                    level.setBlockAndUpdate(otherPos, blockState.setValue(SEGMENT, i))
+                }
+            }
+
+            return true
+        }
+
         val SEGMENT: IntegerProperty = IntegerProperty.create("segment", 0, 7)
         val SHAPE: VoxelShape = box(0.0, 0.0, 0.0, 16.0, 1.0, 16.0)
         val CODEC: MapCodec<CityRugBlock> = simpleCodec(::CityRugBlock)
