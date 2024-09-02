cc6806eb38a8621afa7bd8d5d35a0af94fbc54e1
Aaron Howser <aaronhowser1@gmail.com>
2024-09-02 13:16:03 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PogoStickItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PogoStickItem.kt
@@ -1,10 +1,17 @@
 package dev.aaronhowser.mods.paracosm.item
 
+import dev.aaronhowser.mods.paracosm.datagen.tag.ModBlockTagsProvider
+import dev.aaronhowser.mods.paracosm.packet.ModPacketHandler
+import dev.aaronhowser.mods.paracosm.packet.server_to_client.TellClientsUsedPogo
 import dev.aaronhowser.mods.paracosm.registry.ModItems
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.phys.AABB
 
 class PogoStickItem(
     properties: Properties = Properties()
@@ -24,6 +31,60 @@ class PogoStickItem(
             }
 
             return null
+        }
+
+        fun usePogo(player: ServerPlayer) {
+            val heldPogo = getHeldPogoStick(player) ?: return
+
+            player.playSound(SoundEvents.PISTON_EXTEND)
+
+            val packet = TellClientsUsedPogo(player)
+            ModPacketHandler.messageNearbyPlayers(
+                packet,
+                player.level() as ServerLevel,
+                player.eyePosition,
+                64.0
+            )
+
+            var damageItemAmount = 0
+
+            //TODO: Configurable, upgradeable?
+            val damage = minOf(player.fallDistance, 6f)
+
+            if (player.fallDistance >= 2f) {
+
+                val box = AABB(
+                    player.position().x - 0.3,
+                    player.position().y - 0.0,
+                    player.position().z - 0.3,
+                    player.position().x + 0.3,
+                    player.position().y + 0.6,
+                    player.position().z + 0.3
+                )
+
+                for (entity in player.level().getEntities(player, box)) {
+                    if (entity == player) continue
+
+                    entity.hurt(player.level().damageSources().flyIntoWall(), damage)
+                    damageItemAmount++
+                }
+            }
+
+            val shouldFallDamageItem =
+                !player.isCreative
+                        && player.fallDistance > 1f
+                        && !player.level()
+                    .getBlockState(player.blockPosition().below())
+                    .`is`(ModBlockTagsProvider.POGO_BOOST)
+
+            if (shouldFallDamageItem) damageItemAmount++
+
+            if (damageItemAmount > 0) {
+                val equipmentSlot = player.getEquipmentSlotForItem(heldPogo)
+                heldPogo.hurtAndBreak(damageItemAmount, player, equipmentSlot)
+            }
+
+            player.fallDistance = 0f
         }
 
     }
