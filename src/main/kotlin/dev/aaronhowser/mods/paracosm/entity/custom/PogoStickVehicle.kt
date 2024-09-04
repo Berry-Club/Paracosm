c26c2d2c8032e1cfd48d51ab84c32fb2116f87fa
Aaron Howser <aaronhowser1@gmail.com>
2024-09-04 10:48:23 -0500

    progress

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/PogoStickVehicle.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/custom/PogoStickVehicle.kt
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.EntityDimensions
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.entity.vehicle.Boat
 import net.minecraft.world.entity.vehicle.VehicleEntity
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.Level
@@ -82,7 +83,7 @@ class PogoStickVehicle(
     }
 
     override fun canCollideWith(entity: Entity): Boolean {
-        return this.controllingPassenger != entity
+        return Boat.canVehicleCollide(this, entity)
     }
 
     override fun isPushable(): Boolean {
