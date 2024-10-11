d2d20c6402c754edb05c167b9ae1fd671e3c6082
Aaron Howser <aaronhowser1@gmail.com>
2024-10-11 15:47:06 -0500

    refactor

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/event/ClientModBusEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/event/ClientModBusEvents.kt
@@ -42,20 +42,20 @@ object ClientModBusEvents {
     @SubscribeEvent
     fun onClientSetup(event: FMLClientSetupEvent) {
 
-        fun <T : Entity> register(
+        fun <T : Entity> registerEntityRenderer(
             entityHolder: DeferredHolder<EntityType<*>, EntityType<T>>,
             provider: EntityRendererProvider<T>
         ) {
             EntityRenderers.register(entityHolder.get(), provider)
         }
 
-        register(ModEntityTypes.TEDDY_BEAR, ::TeddyBearRenderer)
-        register(ModEntityTypes.STRING_WORM, ::StringWormRenderer)
-        register(ModEntityTypes.AARONBERRY, ::AaronberryRenderer)
-        register(ModEntityTypes.DODGEBALL, ::ThrownItemRenderer)
-        register(ModEntityTypes.SHRINK_RAY_PROJECTILE, ::ShrinkRayProjectileRenderer)
-        register(ModEntityTypes.STICKY_HAND_PROJECTILE, ::StickyHandProjectileRenderer)
-        register(ModEntityTypes.POGO_STICK_VEHICLE, ::PogoStickVehicleRenderer)
+        registerEntityRenderer(ModEntityTypes.TEDDY_BEAR, ::TeddyBearRenderer)
+        registerEntityRenderer(ModEntityTypes.STRING_WORM, ::StringWormRenderer)
+        registerEntityRenderer(ModEntityTypes.AARONBERRY, ::AaronberryRenderer)
+        registerEntityRenderer(ModEntityTypes.DODGEBALL, ::ThrownItemRenderer)
+        registerEntityRenderer(ModEntityTypes.SHRINK_RAY_PROJECTILE, ::ShrinkRayProjectileRenderer)
+        registerEntityRenderer(ModEntityTypes.STICKY_HAND_PROJECTILE, ::StickyHandProjectileRenderer)
+        registerEntityRenderer(ModEntityTypes.POGO_STICK_VEHICLE, ::PogoStickVehicleRenderer)
 
         CuriosRendererRegistry.register(
             ModItems.SEEING_STONE.get(),
