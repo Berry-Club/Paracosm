876ab6a53a6a37810c2d5e866a14eaa49f8d108a
aaron <aaronhowser1@gmail.com>
2025-11-17 21:39:37 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/block/block_entity/CityRugBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/block/block_entity/CityRugBlockEntity.kt
@@ -4,8 +4,8 @@ import dev.aaronhowser.mods.paracosm.registry.ModBlockEntities
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.IntArrayTag
 import net.minecraft.nbt.ListTag
+import net.minecraft.nbt.LongTag
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
@@ -17,56 +17,46 @@ class CityRugBlockEntity(
 	var origin: BlockPos? = null
 	var childBlocks: List<BlockPos> = emptyList()
 
-	companion object {
-		const val ORIGIN_NBT = "Origin"
-		const val CHILD_BLOCKS_NBT = "ChildBlocks"
-	}
-
 	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.saveAdditional(tag, registries)
 
 		val origin = origin
 		if (origin != null) {
-			tag.putIntArray(ORIGIN_NBT, intArrayOf(origin.x, origin.y, origin.z))
+			tag.putLong(ORIGIN_NBT, origin.asLong())
 		}
 
 		if (childBlocks.isNotEmpty()) {
-			val list = tag.getList(CHILD_BLOCKS_NBT, ListTag.TAG_INT_ARRAY.toInt())
+			val list = tag.getList(CHILD_BLOCKS_NBT, ListTag.TAG_LONG.toInt())
 
 			for (childPos in childBlocks) {
-				val array = intArrayOf(childPos.x, childPos.y, childPos.z)
-				val intArrayTag = IntArrayTag(array)
-				list.add(intArrayTag)
+				list.add(LongTag.valueOf(childPos.asLong()))
 			}
 
 			tag.put(CHILD_BLOCKS_NBT, list)
 		}
-
 	}
 
 	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.loadAdditional(tag, registries)
 
 		if (tag.contains(ORIGIN_NBT)) {
-			val originArray = tag.getIntArray(ORIGIN_NBT)
-			origin = BlockPos(originArray[0], originArray[1], originArray[2])
+			val originLong = tag.getLong(ORIGIN_NBT)
+			origin = BlockPos.of(originLong)
 		}
 
 		if (tag.contains(CHILD_BLOCKS_NBT)) {
-			val list = tag.getList(CHILD_BLOCKS_NBT, ListTag.TAG_INT_ARRAY.toInt())
+			val list = tag.getList(CHILD_BLOCKS_NBT, ListTag.TAG_LONG.toInt())
 
 			for (i in 0 until list.size) {
-				val array = list[i] as IntArrayTag
-
-				val x = array[0].asInt
-				val y = array[1].asInt
-				val z = array[2].asInt
-
-				val childPos = BlockPos(x, y, z)
-				childBlocks += childPos
+				val longTag = list[i] as? LongTag ?: continue
+				childBlocks += BlockPos.of(longTag.asLong)
 			}
 		}
+	}
 
+	companion object {
+		const val ORIGIN_NBT = "Origin"
+		const val CHILD_BLOCKS_NBT = "ChildBlocks"
 	}
 
 }
