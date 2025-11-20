ad3653276196120c4ddb890285fdbfbf57dd2883
aaron <aaronhowser1@gmail.com>
2025-11-20 14:24:08 -0600

    fix

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/research/ResearchType.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/research/ResearchType.kt
@@ -1,13 +1,21 @@
 package dev.aaronhowser.mods.paracosm.research
 
 import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 
 data class ResearchType(
 	val color: Int
 ) {
 
 	companion object {
-		val DIRECT_CODEC: Codec<ResearchType> = Codec.INT.xmap(::ResearchType, ResearchType::color)
+		val DIRECT_CODEC: Codec<ResearchType> =
+			RecordCodecBuilder.create { instance ->
+				instance.group(
+					Codec.INT
+						.fieldOf("color")
+						.forGetter(ResearchType::color)
+				).apply(instance, ::ResearchType)
+			}
 	}
 
 }
