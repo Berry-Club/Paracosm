833b694db77da56c449f23ba634190da06ba34d5
aaron <aaronhowser1@gmail.com>
2025-11-25 18:23:49 -0600

    feat: enhance FoamDartProjectile with pickup item functionality and hit behavior

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/FoamDartProjectile.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/FoamDartProjectile.kt
@@ -1,24 +1,39 @@
 package dev.aaronhowser.mods.paracosm.entity
 
+import dev.aaronhowser.mods.aaron.AaronExtensions.isServerSide
 import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.projectile.Projectile
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.ButtonBlock
 import net.minecraft.world.level.block.DoorBlock
 import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.HitResult
 
 open class FoamDartProjectile(
 	entityType: EntityType<out FoamDartProjectile>,
 	level: Level
 ) : Projectile(entityType, level) {
 
-	private var owner: LivingEntity? = null
+	protected var owner: LivingEntity? = null
+	protected var pickupItemStack: ItemStack? = null
 
 	override fun defineSynchedData(builder: SynchedEntityData.Builder) {
 		// none
+	}
+
+	override fun onHit(result: HitResult) {
+		super.onHit(result)
+
+		if (isServerSide) {
+			if (pickupItemStack != null) {
+				spawnAtLocation(pickupItemStack!!)
+			}
+			discard()
+		}
 	}
 
 	override fun onHitBlock(result: BlockHitResult) {
@@ -64,5 +79,13 @@ Smoke cloud?
 Homing? Richochet?
 
 BANG flag that's not actually a dart and just refills your whimsy or whatever
+
+Suction cup dart type?
+Sticks into blocks and entities like an arrow
+Could be the base of some other upgrades?
+
+Weighted suction dart
+Slows down entities
+Gives blocks gravity?
 
 */
