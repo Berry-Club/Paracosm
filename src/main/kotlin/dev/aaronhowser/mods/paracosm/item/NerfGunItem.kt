1d7e813f84c439d38dbe6a74dc3d07d167fdd06b
aaron <aaronhowser1@gmail.com>
2025-11-26 07:17:34 -0600

    feat: implement Nerf Gun functionality with projectile shooting mechanics

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/NerfGunItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/NerfGunItem.kt
@@ -1,8 +1,39 @@
 package dev.aaronhowser.mods.paracosm.item
 
-import net.minecraft.world.item.Item
+import dev.aaronhowser.mods.paracosm.registry.ModItems
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.projectile.Projectile
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.ProjectileWeaponItem
+import java.util.function.Predicate
 
-class NerfGunItem(properties: Properties) : Item(properties) {
+class NerfGunItem(properties: Properties) : ProjectileWeaponItem(properties) {
+
+	@Deprecated("Deprecated in Java")
+	override fun getAllSupportedProjectiles(): Predicate<ItemStack> {
+		return Predicate { stack: ItemStack -> stack.`is`(ModItems.FOAM_DART) }
+	}
+
+	override fun getDefaultProjectileRange(): Int = 15
+
+	override fun shootProjectile(
+		shooter: LivingEntity,
+		projectile: Projectile,
+		index: Int,
+		velocity: Float,
+		inaccuracy: Float,
+		angle: Float,
+		target: LivingEntity?
+	) {
+		projectile.shootFromRotation(
+			shooter,
+			shooter.xRot,
+			shooter.yRot + angle,
+			0f,
+			velocity,
+			inaccuracy
+		)
+	}
 
 	companion object {
 		val DEFAULT_PROPERTIES: Properties =
