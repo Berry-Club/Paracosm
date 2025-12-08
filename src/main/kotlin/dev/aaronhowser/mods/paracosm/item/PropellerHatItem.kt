e83231aabbb96931ba0e1bde04ed9d75ec556726
aaron <aaronhowser1@gmail.com>
2025-12-07 22:59:33 -0600

    feat: add inventoryTick method to PropellerHatItem for upward movement effect

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PropellerHatItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PropellerHatItem.kt
@@ -1,13 +1,25 @@
 package dev.aaronhowser.mods.paracosm.item
 
-import net.minecraft.core.component.DataComponents
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.item.Equipable
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.minecraft.world.phys.Vec3
 
 class PropellerHatItem(properties: Properties) : Item(properties), Equipable {
 
 	override fun getEquipmentSlot(): EquipmentSlot = EquipmentSlot.HEAD
+
+	override fun inventoryTick(stack: ItemStack, level: Level, entity: Entity, slotId: Int, isSelected: Boolean) {
+		if (entity !is LivingEntity) return
+		val headItem = entity.getItemBySlot(EquipmentSlot.HEAD)
+		if (headItem != stack) return
+
+		entity.addDeltaMovement(Vec3(0.0, 0.5, 0.0))
+	}
 
 	companion object {
 		val DEFAULT_PROPERTIES: Properties = Properties()
