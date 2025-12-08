c769d6ca15973c7c13e4cb9efd1ffef5f1e2cf5c
aaron <aaronhowser1@gmail.com>
2025-12-07 23:00:36 -0600

    cleanup

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PropellerHatItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/PropellerHatItem.kt
@@ -4,8 +4,6 @@ import dev.aaronhowser.mods.paracosm.item.base.WearableItem
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.item.Equipable
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.Vec3
--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/base/WearableItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/base/WearableItem.kt
@@ -3,5 +3,4 @@ package dev.aaronhowser.mods.paracosm.item.base
 import net.minecraft.world.item.Equipable
 import net.minecraft.world.item.Item
 
-abstract class WearableItem(properties: Properties) : Item(properties), Equipable {
-}
+abstract class WearableItem(properties: Properties) : Item(properties), Equipable
