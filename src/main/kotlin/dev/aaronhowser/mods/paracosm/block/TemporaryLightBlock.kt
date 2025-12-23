0d1aad98e91093a856241afdbf1950925ce812ce
aaron <aaronhowser1@gmail.com>
2025-12-22 19:58:11 -0600

    feat: add TemporaryLightBlock and LightUpShoesItem with light emission functionality

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/block/TemporaryLightBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/block/TemporaryLightBlock.kt
@@ -1,12 +1,16 @@
 package dev.aaronhowser.mods.paracosm.block
 
+import dev.aaronhowser.mods.paracosm.registry.ModItems
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.RandomSource
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.AirBlock
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.AABB
 
 class TemporaryLightBlock : AirBlock(
 	Properties.ofFullCopy(Blocks.AIR)
@@ -15,13 +19,19 @@ class TemporaryLightBlock : AirBlock(
 
 	override fun onPlace(state: BlockState, level: Level, pos: BlockPos, oldState: BlockState, movedByPiston: Boolean) {
 		super.onPlace(state, level, pos, oldState, movedByPiston)
-		level.scheduleTick(pos, this, 20)
+		level.scheduleTick(pos, this, 3)
 	}
 
 	override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
 		super.tick(state, level, pos, random)
 		if (level.getBlockState(pos).`is`(this)) {
 			level.removeBlock(pos, false)
+
+			val entitiesInPos = level.getEntitiesOfClass(LivingEntity::class.java, AABB(pos))
+			val isWearingShoes = entitiesInPos.any { it.getItemBySlot(EquipmentSlot.FEET).`is`(ModItems.LIGHT_UP_SHOES) }
+			if (isWearingShoes) {
+				level.setBlock(pos, this.defaultBlockState(), UPDATE_CLIENTS)
+			}
 		}
 	}
 
