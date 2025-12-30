98b2cadbe80d59bb3ca89c03fecd45e04c3ab39d
aaron <aaronhowser1@gmail.com>
2025-12-30 00:29:15 -0600

    refactor ToySoldierGunnerEntity to use secondary constructor for improved initialization

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/ToySoldierGunnerEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/ToySoldierGunnerEntity.kt
@@ -2,10 +2,20 @@ package dev.aaronhowser.mods.paracosm.entity
 
 import dev.aaronhowser.mods.paracosm.entity.base.ToySoldierEntity
 import dev.aaronhowser.mods.paracosm.registry.ModEntityTypes
+import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 import software.bernie.geckolib.animation.AnimatableManager
 
-class ToySoldierGunnerEntity(level: Level) : ToySoldierEntity(ModEntityTypes.TOY_SOLDIER_GUNNER.get(), level) {
+class ToySoldierGunnerEntity : ToySoldierEntity {
+
+	constructor(
+		level: Level
+	) : this(ModEntityTypes.TOY_SOLDIER_GUNNER.get(), level)
+
+	constructor(
+		entityType: EntityType<ToySoldierGunnerEntity>,
+		level: Level
+	) : super(entityType, level)
 
 	override val requiredWhimsy: Double = 10.0
 	override fun registerControllers(controllers: AnimatableManager.ControllerRegistrar) {}
