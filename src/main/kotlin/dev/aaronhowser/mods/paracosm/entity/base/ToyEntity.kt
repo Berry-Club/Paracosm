c6b79b5834923529b30b3555969826e552e45b6c
aaron <aaronhowser1@gmail.com>
2025-12-30 00:13:33 -0600

    refactor ToyEntity to extend LivingEntity and implement OwnableEntity

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/base/ToyEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/entity/base/ToyEntity.kt
@@ -1,38 +1,45 @@
 package dev.aaronhowser.mods.paracosm.entity.base
 
 import dev.aaronhowser.mods.paracosm.attachment.RequiresWhimsy
-import net.minecraft.server.level.ServerLevel
-import net.minecraft.world.entity.AgeableMob
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.world.entity.EntityType
-import net.minecraft.world.entity.TamableAnimal
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.OwnableEntity
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import software.bernie.geckolib.animatable.GeoEntity
+import java.util.*
 
 abstract class ToyEntity(
-	entityType: EntityType<out TamableAnimal>,
+	entityType: EntityType<out ToyEntity>,
 	level: Level
-) : TamableAnimal(entityType, level), RequiresWhimsy, GeoEntity {
+) : LivingEntity(entityType, level), OwnableEntity, RequiresWhimsy, GeoEntity {
 
-	fun hidingFromPlayers(): List<Player> {
-		return super.hidingFromPlayers(level(), eyePosition)
+	var isHiding: Boolean = false
+		private set
+
+	override fun getOwnerUUID(): UUID? {
+		return entityData.get(DATA_OWNER_UUID).orElse(null)
 	}
 
-	override fun getBreedOffspring(p0: ServerLevel, p1: AgeableMob): AgeableMob? {
-		return null
-	}
-
-	override fun isFood(p0: ItemStack): Boolean {
-		return false
-	}
+	fun isOwnedBy(livingEntity: LivingEntity):
 
 	override fun tick() {
 		super.tick()
 		isHiding = super.isHiding(level(), eyePosition)
 	}
 
-	var isHiding: Boolean = false
-		private set
+	fun hidingFromPlayers(): List<Player> {
+		return super.hidingFromPlayers(level(), eyePosition)
+	}
+
+	companion object {
+		val DATA_OWNER_UUID: EntityDataAccessor<Optional<UUID>> =
+			SynchedEntityData.defineId(ToyEntity::class.java, EntityDataSerializers.OPTIONAL_UUID)
+
+
+	}
 
 }
