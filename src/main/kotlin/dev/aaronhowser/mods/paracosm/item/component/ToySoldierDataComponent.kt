b27d7bc50805a3bcd81ccef071474a3b9a99b0b8
aaron <aaronhowser1@gmail.com>
2025-12-30 03:37:58 -0600

    add spawnAt method to ToySoldierDataComponent for entity instantiation

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/item/component/ToySoldierDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/item/component/ToySoldierDataComponent.kt
@@ -4,13 +4,17 @@ import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.EitherCodec
 import io.netty.buffer.ByteBuf
+import net.minecraft.core.Position
+import net.minecraft.core.registries.Registries
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.item.component.CustomData
+import net.minecraft.world.level.Level
 import net.neoforged.neoforge.registries.DeferredHolder
 
 data class ToySoldierDataComponent(
@@ -20,6 +24,48 @@ data class ToySoldierDataComponent(
 	constructor(deferredEntityType: DeferredHolder<EntityType<*>, EntityType<*>>) : this(deferredEntityType.id)
 	constructor(location: ResourceLocation) : this(Either.left(location))
 	constructor(data: CustomData) : this(Either.right(data))
+
+	fun spawnAt(level: Level, position: Position): Boolean {
+		var result = false
+
+		either
+			.ifLeft { location ->
+				result = spawnFromType(level, position, location)
+			}
+			.ifRight { customData ->
+				result = spawnFromData(level, position, customData)
+			}
+
+		return result
+	}
+
+	private fun spawnFromData(level: Level, position: Position, data: CustomData): Boolean {
+		val typeLocation = ResourceLocation.parse(data.copyTag().getString("id"))
+		val entity = getEntity(level, typeLocation) ?: return false
+
+		data.loadInto(entity)
+
+		entity.moveTo(position.x(), position.y(), position.z())
+		level.addFreshEntity(entity)
+
+		return true
+	}
+
+	private fun spawnFromType(level: Level, position: Position, typeLocation: ResourceLocation): Boolean {
+		val entity = getEntity(level, typeLocation) ?: return false
+
+		entity.moveTo(position.x(), position.y(), position.z())
+		level.addFreshEntity(entity)
+
+		return true
+	}
+
+	private fun getEntity(level: Level, typeRl: ResourceLocation): Entity? {
+		return level.registryAccess()
+			.registryOrThrow(Registries.ENTITY_TYPE)
+			.get(typeRl)
+			?.create(level)
+	}
 
 	companion object {
 		val CODEC: Codec<ToySoldierDataComponent> =
