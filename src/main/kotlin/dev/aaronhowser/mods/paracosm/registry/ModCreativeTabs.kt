d263ddcb84fc97e5aade90372f8d5761515e3ca4
aaron <aaronhowser1@gmail.com>
2026-01-01 03:51:44 -0600

    feat: update creative tab display logic to include Toy Soldier Bucket with default items

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModCreativeTabs.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModCreativeTabs.kt
@@ -1,12 +1,15 @@
 package dev.aaronhowser.mods.paracosm.registry
 
+import dev.aaronhowser.mods.aaron.AaronExtensions.withComponent
 import dev.aaronhowser.mods.paracosm.Paracosm
 import dev.aaronhowser.mods.paracosm.datagen.ModLanguageProvider.Companion.toComponent
 import dev.aaronhowser.mods.paracosm.datagen.language.ModItemLang
+import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.world.item.CreativeModeTab
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.component.ItemContainerContents
 import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredItem
 import net.neoforged.neoforge.registries.DeferredRegister
 import java.util.function.Supplier
 
@@ -20,8 +23,30 @@ object ModCreativeTabs {
 			CreativeModeTab.builder()
 				.title(ModItemLang.CREATIVE_TAB.toComponent())
 				.icon { ModItems.COTTON.toStack() }
-				.displayItems { a, b ->
-					b.acceptAll(ModItems.ITEM_REGISTRY.entries.map { (it as DeferredItem).toStack() })
+				.displayItems { displayContext, output ->
+					val itemsToSkip = listOf(
+						ModItems.TOY_SOLDIER_BUCKET.get()
+					).toSet()
+
+					val allItems = ModItems.ITEM_REGISTRY.entries.map(DeferredHolder<Item, out Item>::get)
+
+					output.acceptAll((allItems - itemsToSkip).map(Item::getDefaultInstance))
+
+					output.accept(
+						ModItems.TOY_SOLDIER_BUCKET
+							.withComponent(
+								DataComponents.CONTAINER,
+								ItemContainerContents.fromItems(
+									listOf(
+										ModItems.TOY_SOLDIER.get().defaultInstance,
+										ModItems.TOY_SOLDIER.get().defaultInstance,
+										ModItems.TOY_SOLDIER.get().defaultInstance,
+										ModItems.TOY_SOLDIER.get().defaultInstance,
+									)
+								)
+							)
+					)
+
 				}
 				.build()
 		})
