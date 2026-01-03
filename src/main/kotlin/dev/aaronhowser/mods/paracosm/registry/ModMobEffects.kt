3ff50ad3a7424201729358518f0e711c1eb1f6d3
aaron <aaronhowser1@gmail.com>
2026-01-03 07:15:20 -0600

    feat: add Aphantasia mob effect with attribute modifier

--- src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModMobEffects.kt
+++ src/main/kotlin/dev/aaronhowser/mods/paracosm/registry/ModMobEffects.kt
@@ -4,6 +4,8 @@ import dev.aaronhowser.mods.aaron.registry.AaronMobEffectsRegistry
 import dev.aaronhowser.mods.paracosm.Paracosm
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.effect.MobEffect
+import net.minecraft.world.effect.MobEffectCategory
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
 import net.neoforged.neoforge.registries.DeferredRegister
 
 object ModMobEffects : AaronMobEffectsRegistry() {
@@ -12,5 +14,16 @@ object ModMobEffects : AaronMobEffectsRegistry() {
 		DeferredRegister.create(Registries.MOB_EFFECT, Paracosm.MOD_ID)
 
 	override fun getMobEffectRegistry(): DeferredRegister<MobEffect> = EFFECT_REGISTRY
+
+	val APHANTASIA =
+		register("aphantasia") {
+			MobEffect(MobEffectCategory.HARMFUL, 0x111111)
+				.addAttributeModifier(
+					ModAttributes.WHIMSY,
+					Paracosm.modResource("aphantasia"),
+					-0.25,
+					AttributeModifier.Operation.ADD_MULTIPLIED_BASE
+				)
+		}
 
 }
